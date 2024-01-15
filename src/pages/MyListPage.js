ac0c3211aef9a7fd1ea21eac6b227d7d2633f6bf
ermankaval <ermankaval@yahoo.com>
2024-01-15 09:56:28 +0300

    denemee
    de

--- src/pages/MyListPage.js
+++ src/pages/MyListPage.js
@@ -1,17 +1,23 @@
 import React, { useState } from "react";
 import Thumbnail2 from "./Thumbnail2";
+import Navbar from "@/components/Navbar";
 
 const MyListPage = ({ wishlist }) => {
-  return (
-    <div>
-      <h1>My List</h1>
-      <div className="wishlist">
-        {wishlist.map((movie) => (
-          <Thumbnail2 key={movie.id} movie={movie} />
-        ))}
-      </div>
-    </div>
-  );
+    return (
+        <>
+            <Navbar />
+            <div>
+                <h1>My List</h1>
+                <div className="wishlist">
+                    {wishlist.map((movie) => (
+                        <Thumbnail2 key={movie.id} movie={movie} />
+                    ))}
+                </div>
+            </div>
+
+        </>
+
+    );
 };
 
 export default MyListPage;
